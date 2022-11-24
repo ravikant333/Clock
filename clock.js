32419f9da2b9e28cfc7df0450ce27c62824fe349
ravikant333 <118275688+ravikant333@users.noreply.github.com>
2022-11-24 18:38:54 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,13 +1,15 @@
+function gettime() {
+  const time = new Date();
+  let hrs = parseInt(time.getHours());
+  let min = parseInt(time.getMinutes());
+  let sec = parseInt(time.getSeconds());
 
-function gettime(){
-    const time= new Date()
-    const hrs= time.getHours()
-    const min= time.getMinutes()
-    const sec=time.getSeconds()
-    
-    document.getElementById("hrs").innerHTML=hrs
-    document.getElementById("min").innerHTML=min
-    document.getElementById('sec').innerHTML=sec
+  if (hrs < 10) hrs = "0" + hrs;
+  if (min < 10) min = "0" + min;
+  if (sec < 10) sec = "0" + sec;
+  document.getElementById("hrs").innerHTML = hrs;
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("sec").innerHTML = sec;
 }
 
-setInterval(gettime,100)
+setInterval(gettime, 100);
